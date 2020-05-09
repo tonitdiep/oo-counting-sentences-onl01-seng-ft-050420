0173588575e8b3f25a5a40bb5fbff9d81dd8476e
Toni  Diep <adiep123@gmail.com>
2020-05-09 17:17:06 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
+
 require 'pry'
 
 class String
 
   def sentence?
-    
-  end
+    self.end_with?(".") 
+end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(" ").count
+    self.split(/[.?!]+/).count
+  # binding.pry
   end
+  
 end
